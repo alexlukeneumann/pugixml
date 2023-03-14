6470c74a812155f3be7528fd8cdef13f4b1a3d84
Alex Neumann <alexlukeneumann95@gmail.com>
2023-03-14 09:48:56 +0000

    Updated premake

--- premake5.lua
+++ premake5.lua
@@ -4,15 +4,14 @@ project "pugixml"
     kind "StaticLib"
     language "C++"
     cppdialect "C++17"
-    targetdir "bin/pugixml/%{cfg.buildcfg}"
-    objdir "intermediates/pugixml/%{cfg.buildcfg}"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "intermediates/%{cfg.buildcfg}"
 
     files
     {
-        "src/**.h",
         "src/**.cpp"
     }
-    
+
     includedirs
     {
         "include"
