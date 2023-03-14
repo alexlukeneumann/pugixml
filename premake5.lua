678fc998df1fb7d8b45ce7ea441d250a1b114de1
Alex Neumann <alexlukeneumann95@gmail.com>
2023-03-14 09:30:54 +0000

    Removed premake workspace

--- premake5.lua
+++ premake5.lua
@@ -1,29 +1,5 @@
 #!lua
 
-workspace "pugixml"
-    startproject "Runtime"
-    architecture "x64"
-    staticruntime "off"
-
-    configurations
-    {
-        "Debug",
-        "Release",
-        "Dist"
-    }
-    
-    filter "configurations:Debug"
-        optimize "off"
-        symbols "on"
-
-    filter "configurations:Release"
-        optimize "on"
-        symbols "on"
-
-    filter "configurations:Dist"
-        optimize "on"
-        symbols "off"
-
 project "pugixml"
     kind "StaticLib"
     language "C++"
